da2ee31ae369bf02df5459e00d269578a8bef9d5
Higor Gabriel <higorgabrieldev@gmail.com>
2024-01-26 17:03:30 -0300

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors'); 
 const apiRoutes = require('./api');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+app.use(cors());
 
 app.use('/api', apiRoutes);
 
