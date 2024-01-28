2696711de324d3ea93655869cbc72ec25dab9a21
Higor Gabriel <higorgabrieldev@gmail.com>
2024-01-27 21:59:03 -0300

    added https

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const cors = require('cors'); 
+const https = require('https');
+const fs = require('fs');
+const cors = require('cors');
 const apiRoutes = require('./api');
 
 const app = express();
@@ -7,8 +9,18 @@ const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 
+// Configurar o servidor HTTPS
+const privateKeyPath = '/etc/letsencrypt/live/api.kcharts.live/privkey.pem';
+const certificatePath = '/etc/letsencrypt/live/api.kcharts.live/fullchain.pem';
+
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT} com HTTPS`);
 });
